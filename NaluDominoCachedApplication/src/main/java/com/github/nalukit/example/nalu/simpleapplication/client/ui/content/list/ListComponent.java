e7baeb9d4ff49d54f9e5ebb67353eb464bc87bf9
Frank Hossfeld <frank.hossfeld@googlemail.com>
2018-10-19 19:58:40 +0200

    added new example
    commit d0bea8664eddd50ac99c7a454416f9333ae61e3a
    Author: Frank Hossfeld <frank.hossfeld@googlemail.com>
    Date:   Fri Oct 19 12:45:25 2018 +0200
    
        chaching feature implemented

--- NaluDominoCachedApplication/src/main/java/com/github/nalukit/example/nalu/simpleapplication/client/ui/content/list/ListComponent.java
+++ NaluDominoCachedApplication/src/main/java/com/github/nalukit/example/nalu/simpleapplication/client/ui/content/list/ListComponent.java
@@ -19,9 +19,11 @@ package com.github.nalukit.example.nalu.simpleapplication.client.ui.content.list
 
 import com.github.nalukit.example.nalu.simpleapplication.client.data.model.dto.Person;
 import com.github.nalukit.nalu.client.component.AbstractComponent;
+import elemental2.dom.HTMLDivElement;
 import elemental2.dom.HTMLElement;
 import elemental2.dom.Text;
 import org.dominokit.domino.ui.button.Button;
+import org.dominokit.domino.ui.button.ButtonSize;
 import org.dominokit.domino.ui.cards.Card;
 import org.dominokit.domino.ui.datatable.ColumnConfig;
 import org.dominokit.domino.ui.datatable.DataTable;
@@ -29,6 +31,7 @@ import org.dominokit.domino.ui.datatable.TableConfig;
 import org.dominokit.domino.ui.datatable.store.LocalListDataStore;
 import org.dominokit.domino.ui.grid.Column;
 import org.dominokit.domino.ui.grid.Row;
+import org.jboss.gwt.elemento.core.Elements;
 import org.jboss.gwt.elemento.core.EventType;
 
 import java.util.ArrayList;
@@ -40,15 +43,53 @@ public class ListComponent
     extends AbstractComponent<IListComponent.Controller, HTMLElement>
     implements IListComponent {
 
-  private DataTable<Person>          table;
+  private final static String CACHED_TEXT = "The controller and the component are cached. To see the caching, select a person by clicking it's name, change something and save the data. The component shows the old, not updated data. Pressing reload will update the list and now you can see the updated data.";
+
+  private final static String NOT_CACHED_TEXT = "The controller and the component are not cached. You will always see the updated data.";
+
+  private DataTable<Person> table;
 
   private LocalListDataStore<Person> store;
+
+  private Button buttonStoreInCache;
+
+  private Button buttonRemvoeFromCache;
+
+  private Button buttonReload;
+
+  private Text infoText;
 
   public ListComponent() {
   }
 
   @Override
   public void render() {
+    this.buttonReload = Button.createPrimary("Reload")
+                              .linkify()
+                              .setSize(ButtonSize.LARGE)
+                              .style()
+                              .setMinWidth("120px")
+                              .get()
+                              .addClickListener(e -> getController().doReload());
+    this.buttonStoreInCache = Button.create("Store in Chache")
+                                    .setSize(ButtonSize.LARGE)
+                                    .style()
+                                    .setMinWidth("120px")
+                                    .setMarginRight("12px")
+                                    .get()
+                                    .addClickListener(e -> getController().doStoreControllerInCache());
+    this.buttonRemvoeFromCache = Button.create("Remove from Cache")
+                                       .setSize(ButtonSize.LARGE)
+                                       .style()
+                                       .setMarginRight("12px")
+                                       .setMinWidth("120px")
+                                       .get()
+                                       .addClickListener(e -> getController().doRemoveControllerfromCache());
+    this.buttonRemvoeFromCache.setDisabled(true);
+
+    HTMLDivElement divElement = Elements.div()
+                                        .asElement();
+
     TableConfig<Person> tableConfig = new TableConfig<>();
     tableConfig.addColumn(ColumnConfig.<Person>create("name",
                                                       "Name").setCellRenderer(cell -> a().textContent(cell.getTableRow()
@@ -89,12 +130,28 @@ public class ListComponent
     this.table = new DataTable<>(tableConfig,
                                  store);
 
-    initElement(Card.create("SEARCH RESULTS")
-                    .appendChild(Row.create()
-                                    .appendChild(Column.span12()
-                                                       .appendChild(this.table)))
-                    .asElement());
+    divElement.appendChild(Card.create("SEARCH RESULTS")
+                               .appendChild(Row.create()
+                                               .appendChild(Column.span12()
+                                                                  .appendChild(this.table)))
+                               .asElement());
 
+    this.infoText = new Text();
+
+    divElement.appendChild(Card.create("Tool Bar")
+                               .appendChild(Row.create()
+                                               .setGap("10px")
+                                               .addColumn(Column.span12()
+                                                                .appendChild(this.infoText))
+                                               .addColumn(Column.span12()
+                                                                .appendChild(this.buttonStoreInCache)
+                                                                .appendChild(this.buttonRemvoeFromCache)
+                                                                .appendChild(this.buttonReload))
+                                               .style()
+                                               .setTextAlign("center"))
+                               .asElement());
+
+    initElement(divElement);
   }
 
   @Override
@@ -107,5 +164,16 @@ public class ListComponent
     resetTable();
     this.store.setData(result);
     this.table.load();
+  }
+
+  @Override
+  public void handleToggleButton(boolean cached) {
+    this.buttonStoreInCache.setDisabled(cached);
+    this.buttonRemvoeFromCache.setDisabled(!cached);
+    if (cached) {
+      this.infoText.textContent = ListComponent.CACHED_TEXT;
+    } else {
+      this.infoText.textContent = ListComponent.NOT_CACHED_TEXT;
+    }
   }
 }
