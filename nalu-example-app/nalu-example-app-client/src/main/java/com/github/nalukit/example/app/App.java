973d7c2be89fa39ab08f93b9cc5e84a381d159e7
Frank Hossfeld <frank.hossfeld@googlemail.com>
2023-12-29 10:50:47 +0100

    intermezzo

---
+++ nalu-example-app/nalu-example-app-client/src/main/java/com/github/nalukit/example/app/App.java
@@ -0,0 +1,18 @@
+package com.github.nalukit.example.app;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class App
+    implements EntryPoint {
+
+  /**
+   * This is the entry point method.
+   */
+  public void onModuleLoad() {
+    GWT.debugger();
+  }
+}
---
+++ nalu-example-app/nalu-example-app-server/src/main/java/com/github/nalukit/example/app/Application.java
@@ -0,0 +1,24 @@
+package com.github.nalukit.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@ServletComponentScan
+public class Application
+    extends SpringBootServletInitializer {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class,
+                          args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(Application.class);
+  }
+
+}
